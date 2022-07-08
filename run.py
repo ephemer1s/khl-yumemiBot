52dab9e0f55fa8c35757cfcf80b06e414c2739d2
Spica TSUKIMI <haochengvanadis@gmail.com>
2022-07-08 03:12:27 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 import sys
+import logging
+
+
+logging.basicConfig(level='INFO')
+file_path = 'yumemi.log'
+sys.stdout = open(file_path, "w")
+
+
 from yumemiBot import yumemi
 
-
 if __name__ == '__main__':
-    
-    file_path = 'yumemi.log'
-    sys.stdout = open(file_path, "w")
-
-    bot = yumemi
-    bot.run()
+    yumemi.run()
     
